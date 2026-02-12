99a92fc2406627c66256f76b79d33b7bf8f554d2
wolfgang-jules <133794525+wolfgang-jules@users.noreply.github.com>
2026-02-11 21:49:23 -0600

    Refactor code structure for improved readability and maintainability

--- components/footer.js
+++ components/footer.js
@@ -11,7 +11,22 @@ class SiteFooter extends HTMLElement {
               Dirección: 14500 SW 32 ST, Miami, FL 33175<br />
               Email: <a href="mailto:info@postek-usa.com">info@postek-usa.com</a>
             </p>
-            <p class="footer__countries">Países de operación: USA, Guatemala, Honduras, Nicaragua, Costa Rica, Panamá</p>
+            <div class="footer__flags" aria-label="Países de operación">
+              <img src="images/flags/us.svg" alt="Estados Unidos" loading="lazy" />
+              <img src="images/flags/mx.svg" alt="México" loading="lazy" />
+              <img src="images/flags/do.svg" alt="República Dominicana" loading="lazy" />
+              <img src="images/flags/pr.svg" alt="Puerto Rico" loading="lazy" />
+              <img src="images/flags/es.svg" alt="España" loading="lazy" />
+              <img src="images/flags/ec.svg" alt="Ecuador" loading="lazy" />
+              <img src="images/flags/bo.svg" alt="Bolivia" loading="lazy" />
+              <img src="images/flags/gt.svg" alt="Guatemala" loading="lazy" />
+              <img src="images/flags/sv.svg" alt="El Salvador" loading="lazy" />
+              <img src="images/flags/bz.svg" alt="Belice" loading="lazy" />
+              <img src="images/flags/hn.svg" alt="Honduras" loading="lazy" />
+              <img src="images/flags/ni.svg" alt="Nicaragua" loading="lazy" />
+              <img src="images/flags/cr.svg" alt="Costa Rica" loading="lazy" />
+              <img src="images/flags/pa.svg" alt="Panamá" loading="lazy" />
+            </div>
           </div>
           <div class="footer__links">
             <a href="index.html">Inicio</a>
